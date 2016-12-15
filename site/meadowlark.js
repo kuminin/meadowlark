84f979e6688098533ede16198e4e290d83cfb23c
kuminin <kumin.in@hotmail.com>
2016-12-15 15:07:51 -0800

    Initial Commit

---
+++ site/meadowlark.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+// set up handlebars view engine.
+const handlebars = require('express-handlebars')
+        .create({ defaultLayout:'main' });
+const app = express();
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple stupid.",
+];
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/about', (req, res) => {
+    var randomFortune =
+            fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
+});
+
+// custom 404 page
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.' );
+});

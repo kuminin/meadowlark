a285ca000c1469fc3bd2f15d6819d1940bad8303
kuminin <kumin.in@hotmail.com>
2016-12-16 15:15:25 -0800

    Finished Chapter 4

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,9 +1,9 @@
-const express = require('express');
+var express = require('express');
 
 // set up handlebars view engine.
-const handlebars = require('express-handlebars')
+var handlebars = require('express-handlebars')
         .create({ defaultLayout:'main' });
-const app = express();
+var app = express();
 
 var fortune = require('./lib/fortune.js');
 
@@ -13,29 +13,47 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
+app.use(function(req,res,next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+            req.query.test === '1';
+    next();
+});
+
+app.get('/', function(req, res) {
     res.render('home');
 });
-app.get('/about', (req, res) => {
+app.get('/about', function(req, res) {
     var randomFortune =
-            fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: fortune.getFortune() });
+            fortune[Math.floor(Math.random() * fortune.length)];
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+app.get('/tours/hood-river', function(req, res) {
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res) {
+    res.render('tours/request-group-rate');
+});
+app.get('/tours/oregon-coast', function(req, res) {
+    res.render('tours/oregon-coast');
 });
 
 // custom 404 page
-app.use((req, res) => {
+app.use(function(req, res) {
     res.status(404);
     res.render('404');
 });
 
 // custom 500 page
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.' );
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
